090463926ff48e9f745f601378d3e828a274035b
Benjamin Vredevelt <bjv753@gmail.com>
2023-10-04 17:35:04 -0500

    update

---
+++ components/index.ts
@@ -0,0 +1,17 @@
+import Hero from './Hero';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import SearchBar from './SearchBar';
+import SearchManufacturer from './SearchManufacturer';
+import CustomFilter from './CustomFilter';
+import CustomButton from './CustomButton';
+
+export { 
+      Hero,
+      CustomButton,
+      Navbar,
+      Footer,
+      SearchBar,
+      CustomFilter,
+      SearchManufacturer
+ }

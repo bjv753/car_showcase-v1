b312b150afc93526036c35cc5d7254039b2ba9da
Benjamin Vredevelt <bjv753@gmail.com>
2023-10-06 13:38:11 -0500

    update

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import { CarProps } from '@/types';
 import CustomButton from './CustomButton';
 import { calculateCarRent } from '@/utils';
-import { CarDetails } from '.';
+import  CarDetails  from './CarDetails';
 
 interface CarCardProps {
       car: CarProps;

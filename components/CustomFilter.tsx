6203a454483e01a2756b755c89ed6df2acb206f3
Benjamin Vredevelt <bjv753@gmail.com>
2023-10-07 14:57:52 -0500

    update

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,6 +1,12 @@
-import { Fragment, useState } from 'react';
+"use client";
 
-const CustomFilter = () => {
+import { Fragment, useState } from 'react';
+import Image from 'next/image';
+import {useRouter } from 'next/navigation';
+import { Listbox, Transition } from '@headlessui/react';
+import { CustomFilterProps } from '@/types';
+
+const CustomFilter = ({ title, options }: CustomFilterProps) => {
   return (
     <div>CustomFilter</div>
   )
